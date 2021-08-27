113574260b9eb9853d6d41a130d8c052db5f4442
Alex Smirnov <sas2job@gmail.com>
2021-08-27 11:42:59 +0300

    Add UseState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-    let likes = 5;
+  const [likes, setLikes] = useState(5)
+  
+  function increment() {
+    setLikes(likes + 1)
+  }
 
-    function increment() {
-      likes += 1;
-      console.log(likes);  
-    }
-    
+  function decrement() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
       <h1>{likes}</h1>
       <button onClick={increment}>Increment</button>
-      <button onClick={() => likes -= 1}>Decrement</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
