9a7e00bcc5cd23901d269289a615d65520e26db0
Alex Smirnov <sas2job@gmail.com>
2021-08-27 12:08:55 +0300

    Add controlled component for input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState(5)
-  
+  const [value, setValue] = useState('Text in input')
+
   function increment() {
     setLikes(likes + 1)
   }
@@ -14,6 +15,11 @@ function App() {
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}/>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
