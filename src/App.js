ef4912597b6942727c9cd075fd5839ed981da8be
Alex Smirnov <sas2job@gmail.com>
2021-08-28 10:57:45 +0300

    Add class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
 import Counter from "./components/Counter";
 function App() {
   const [value, setValue] = useState('Text in input')
 
   return (
     <div className="App">
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
