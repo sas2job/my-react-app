a978888f8c85c7c5c98e02baab72e8f7e55f8cd3
Alex Smirnov <sas2job@gmail.com>
2021-09-05 12:57:26 +0300

    Add ccs style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from "./components/Counter";
+import './styles/App.css';
 function App() {
   const [value, setValue] = useState('Text in input')
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - язык программирования
+          </div>
+        </div>
+        <div className="post__btn">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
