47575f178087f3c4b04b50dd6fcd24a974706c7e
Alex Smirnov <sas2job@gmail.com>
2021-09-11 22:37:33 +0300

    Lists. Array of objects to array of React

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from "./components/Counter";
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import './styles/App.css';
-function App() {
-  const [value, setValue] = useState('Text in input')
 
+function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Ruby on Rails', body: 'Description'},
+    {id: 2, title: 'Javascript', body: 'Description'},
+    {id: 3, title: 'React', body: 'Description'}
+  ])
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Rust', body: 'Description'},
+    {id: 3, title: 'Java', body: 'Description'}
+  ])
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 2, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 3, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 4, title: 'Javascript', body: 'Description'}}/>
-      <PostItem post={{id: 5, title: 'Javascript', body: 'Description'}}/>
+      <PostList posts={posts} title="Список постов 1"/>
+      <PostList posts={posts2} title="Список постов 2"/>
     </div>
   );
 }
